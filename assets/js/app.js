715b801f4ae22eaec45ea9115cd10ce55f71b897
Diana Ampudia <dianalauraampudia@gmail.com>
2024-08-26 14:51:34 -0600

    se agregaron comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
+// se cambio el símbolo de # por el de clase y se coloco un punto donde faltaba
 const $n = document.querySelector('.name');
 const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
+//se coloco el async, se cambiaron las "" por `` y se agrego la línea 12
 async function displayUser(username) {
   $n.textContent = 'cargando...';
-  try{
   const response = await fetch(`${usersEndpoint}/${username}`);
   const data = await response.json();
   console.log(data);
   $n.textContent = `${data.name}`;
   $b.textContent = `${data.blog}`;
   $l.textContent = `${data.location}`;
-  }
-  catch (error){
-    handleError(error);
-  }
 }
 
+//se coloco correctamente el nombre de la variable. Anteriormente estaba como "n"
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
